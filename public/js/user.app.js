760a8e85bb054993e6df622876647e26258c9a7f
Noah Newkirk <nsnewkir@iu.edu>
2019-09-15 18:04:37 -0400

    Randomized User Page using RandomUser api. Made adjustments to css.

---
+++ public/js/user.app.js
@@ -0,0 +1,22 @@
+var userApp = new Vue({
+  el: '#userRandomApp',
+  data: {
+    person:  [
+    {
+
+    }
+  ]
+},
+  methods: {
+      fetchUsers() {
+          fetch('https://randomuser.me/api/')
+          .then( response => response.json() )
+          .then( json => {userApp.person = json.results[0]})
+          ;
+      }
+  },
+  created() {
+    this.fetchUsers();
+  }
+})
+//I worked with Anson Lam, Lindsey Chastain, Riley McGrath, and Brian Emmons
