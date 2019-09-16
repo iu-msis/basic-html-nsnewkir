f607da856321057e5d918e4f1b2102a90a9c3591
Noah Newkirk <nsnewkir@iu.edu>
2019-09-15 21:28:52 -0400

    Added json structure to catch js errors, updated character encoding

--- app/public/js/user.app.js
+++ app/public/js/user.app.js
@@ -1,11 +1,59 @@
 var userApp = new Vue({
   el: '#userRandomApp',
   data: {
-    person:  [
+    person:
     {
-
+            "gender": "",
+            "name": {
+              "title": "",
+              "first": "",
+              "last": ""
+            },
+            "location": {
+              "street": "",
+              "city": "",
+              "state": "",
+              "postcode": "",
+              "coordinates": {
+                "latitude": "",
+                "longitude": ""
+              },
+              "timezone": {
+                "offset": "",
+                "description": ""
+              }
+            },
+            "email": "",
+            "login": {
+              "uuid": "",
+              "username": "",
+              "password": "",
+              "salt": "",
+              "md5": "",
+              "sha1": "",
+              "sha256": ""
+            },
+            "dob": {
+              "date": "",
+              "age": null
+            },
+            "registered": {
+              "date": "",
+              "age": null
+            },
+            "phone": "",
+            "cell": "",
+            "id": {
+              "name": "",
+              "value": ""
+            },
+            "picture": {
+              "large": "",
+              "medium": "",
+              "thumbnail": ""
+            },
+            "nat": "NO"
     }
-  ]
 },
   methods: {
       fetchUsers() {
